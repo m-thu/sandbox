eeba90df92019c3bbd6aa36085fd3c225ce679a1
m-thu <m-thu@users.noreply.github.com>
2016-12-17 13:02:06 +0100

    simplify blink code

--- icestick/blink/blink.v
+++ icestick/blink/blink.v
@@ -1,21 +1,14 @@
 module top (
 	input wire clk,
-	output wire led1,
-	output wire led2,
-	output wire led3,
-	output wire led4,
-	output wire led5
+	output wire led1,led2,led3,led4,led5
 );
 
 	reg [23:0] counter = 0;
 
 	assign led1 = counter[23];
-	assign led2 = 1'b0;
-	assign led3 = 1'b0;
-	assign led4 = 1'b0;
-	assign led5 = 1'b0;
+	assign {led2,led3,led4,led5} = 4'b0000;
 
 	always @(posedge clk)
-		counter <= counter + 1;
+		counter <= counter + 24'b1;
 
 endmodule
